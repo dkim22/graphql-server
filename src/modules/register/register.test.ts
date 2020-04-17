4221f21d668bead46465704ee4c95fe4f8852650
dkim22 <ehddnr1186@gmail.com>
2020-04-17 19:53:58 +0900

    Create validating input

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -2,6 +2,12 @@ import { request } from "graphql-request";
 import { AddressInfo } from "net";
 import { startServer } from "../../startServer";
 import { User } from "../../entity/User";
+import {
+  duplicateEmail,
+  emailNotLongEnough,
+  invalidEmail,
+  passwordNotLongEnough,
+} from "./errorMessages";
 
 let getHost = () => "";
 
@@ -14,9 +20,9 @@ beforeAll(async () => {
 const email = "kim@kim.com";
 const password = "agagag";
 
-const mutation = `
+const mutation = (e: string, p: string) => `
   mutation {
-    register(email: "${email}", password: "${password}") {
+    register(email: "${e}", password: "${p}") {
       path
       message
     }
@@ -24,7 +30,8 @@ const mutation = `
 `;
 
 test("Register user", async () => {
-  const response = await request(getHost(), mutation);
+  // 유저 등록 가능한지 테스트
+  const response = await request(getHost(), mutation(email, password));
   expect(response).toEqual({ register: null });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
@@ -32,16 +39,57 @@ test("Register user", async () => {
   expect(user.email).toEqual(email);
   // 패스워드 해싱 되었는지 확인
   expect(user.password).not.toEqual(password);
-  const response2 = await request(getHost(), mutation);
+
+  // 중복된 이메일인지 테스트
+  const response2 = await request(getHost(), mutation(email, password));
   expect(response2.register).toHaveLength(1);
-  expect(response2.register[0].path).toEqual("email");
-  // message까지 테스트하기에는 너무 빡빡하다.
-  // expect(response2).toEqual({
-  //   register: [
-  //     {
-  //       path: "email",
-  //       message: "already taken",
-  //     },
-  //   ],
-  // });
+  expect(response2.register[0]).toEqual({
+    path: "email",
+    message: duplicateEmail,
+  });
+
+  // 잘못된 이메일인지 테스트
+  const response3 = await request(getHost(), mutation("b", password));
+  expect(response3).toEqual({
+    register: [
+      {
+        path: "email",
+        message: emailNotLongEnough,
+      },
+      {
+        path: "email",
+        message: invalidEmail,
+      },
+    ],
+  });
+
+  // 잘못된 패스워드 인지 테스트
+  const response4 = await request(getHost(), mutation(email, "ad"));
+  expect(response4).toEqual({
+    register: [
+      {
+        path: "password",
+        message: passwordNotLongEnough,
+      },
+    ],
+  });
+
+  // 잘못된 패스워드와 이메일 인지 테스트
+  const response5 = await request(getHost(), mutation("df", "ad"));
+  expect(response5).toEqual({
+    register: [
+      {
+        path: "email",
+        message: emailNotLongEnough,
+      },
+      {
+        path: "email",
+        message: invalidEmail,
+      },
+      {
+        path: "password",
+        message: passwordNotLongEnough,
+      },
+    ],
+  });
 });
